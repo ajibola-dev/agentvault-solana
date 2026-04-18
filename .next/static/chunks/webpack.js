652df9ca03ed2d50c2ca68c1981ff4bc0e93a6fe
ajibola-dev <olalekeajibola93@gmail.com>
2026-04-18 11:23:40 +0100

    fix: exclude migrations and programs from TS build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "10866aa2d87f8334"; }
+/******/ 		__webpack_require__.h = function() { return "2059452336e1efc7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
